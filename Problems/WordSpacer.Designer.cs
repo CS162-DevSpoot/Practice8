d5ea2da024c0f21ab6f91ae7f8cac2d5f4f6f316
DeveloperSpoot <52226518+DeveloperSpoot@users.noreply.github.com>
2024-04-10 15:35:27 -0700

    This stuff is now completed. Rember when working with an array backwards to subtract 1 from the array.Lnegth......

--- Problems/WordSpacer.Designer.cs
+++ Problems/WordSpacer.Designer.cs
@@ -28,12 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "WordSpacer";
+            exitButton = new Button();
+            processButton = new Button();
+            groupBox2 = new GroupBox();
+            displayLabel = new Label();
+            groupBox1 = new GroupBox();
+            stringTextBox = new TextBox();
+            groupBox2.SuspendLayout();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // exitButton
+            // 
+            exitButton.BackColor = Color.Tomato;
+            exitButton.ForeColor = SystemColors.ControlLightLight;
+            exitButton.Location = new Point(12, 157);
+            exitButton.Name = "exitButton";
+            exitButton.Size = new Size(318, 23);
+            exitButton.TabIndex = 7;
+            exitButton.Text = "EXIT";
+            exitButton.UseVisualStyleBackColor = false;
+            exitButton.Click += exitButtonClicked;
+            // 
+            // processButton
+            // 
+            processButton.BackColor = Color.ForestGreen;
+            processButton.ForeColor = SystemColors.ControlLightLight;
+            processButton.Location = new Point(12, 128);
+            processButton.Name = "processButton";
+            processButton.Size = new Size(318, 23);
+            processButton.TabIndex = 6;
+            processButton.Text = "Process";
+            processButton.UseVisualStyleBackColor = false;
+            processButton.Click += processButtonClicked;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(displayLabel);
+            groupBox2.Location = new Point(12, 70);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(318, 52);
+            groupBox2.TabIndex = 5;
+            groupBox2.TabStop = false;
+            // 
+            // displayLabel
+            // 
+            displayLabel.BackColor = SystemColors.ControlLight;
+            displayLabel.Location = new Point(6, 19);
+            displayLabel.Name = "displayLabel";
+            displayLabel.Size = new Size(305, 23);
+            displayLabel.TabIndex = 0;
+            displayLabel.Text = "So it goes'...";
+            displayLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(stringTextBox);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(318, 52);
+            groupBox1.TabIndex = 4;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Enter Your String";
+            // 
+            // stringTextBox
+            // 
+            stringTextBox.Location = new Point(6, 22);
+            stringTextBox.Name = "stringTextBox";
+            stringTextBox.Size = new Size(305, 23);
+            stringTextBox.TabIndex = 0;
+            // 
+            // WordSpacer
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(345, 192);
+            Controls.Add(exitButton);
+            Controls.Add(processButton);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "WordSpacer";
+            Text = "WordSpacer";
+            groupBox2.ResumeLayout(false);
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button exitButton;
+        private Button processButton;
+        private GroupBox groupBox2;
+        private Label displayLabel;
+        private GroupBox groupBox1;
+        private TextBox stringTextBox;
     }
 }
--- Problems/WordSpacer.cs
+++ Problems/WordSpacer.cs
@@ -1,20 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace CS162_Practice8.Problems
+﻿namespace CS162_Practice8.Problems
 {
     public partial class WordSpacer : Form
     {
         public WordSpacer()
         {
             InitializeComponent();
+        }
+
+        private void exitButtonClicked(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void processButtonClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                char[] tokenalize = (stringTextBox.Text).ToCharArray();
+                string newString = "";
+
+                for (int i = 0; i < tokenalize.Length; i++)
+                {
+                    int length = tokenalize.Length - 1;
+                    char character = tokenalize[length - i];
+                    if (char.IsUpper(character)){
+                        newString = $" {character}" + newString;
+                    }
+                    else
+                    {
+                        newString = character + newString;
+                    }
+                }
+
+                displayLabel.Text = newString;
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
